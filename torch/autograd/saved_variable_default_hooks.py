16bd5c610d24c5125ee617c995047bd874d2bd80
Victor Quach <quach@fb.com>
2021-07-28 15:41:45 -0400

    Add default hooks to save tensors on CPU
    Fix #57100
    
    ghstack-source-id: 926ac8c088a97dd1444ee25a9fb66a57792295c6
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/61928

--- torch/autograd/saved_variable_default_hooks.py
+++ torch/autograd/saved_variable_default_hooks.py
@@ -5,3 +5,22 @@ def set_saved_tensors_default_hooks(pack_hook, unpack_hook):
 
 def reset_saved_tensors_default_hooks():
     torch._C._autograd._reset_default_hooks()
+
+def set_save_on_cpu_hooks(pin_memory=False):
+    def pack_hook(tensor):
+        if not pin_memory:
+            return (tensor.device, tensor.cpu())
+
+        storage = torch.empty(
+            tensor.size(),
+            dtype=tensor.dtype,
+            layout=tensor.layout,
+            pin_memory=(torch.cuda.is_available() and not tensor.is_sparse))
+        storage.copy_(tensor)
+        return (tensor.device, storage)
+
+    def unpack_hook(packed):
+        device, tensor = packed
+        return tensor.to(device, non_blocking=pin_memory)
+
+    torch._C._autograd._register_default_hooks(pack_hook, unpack_hook)
